caa6856a6e4150d01a79a0c69f5cc95890626f20
241105060145 <harshilverma012@gmail.com>
2026-04-30 10:41:59 +0530

    Add files via upload

---
+++ ALA3/Main.java
@@ -0,0 +1,7 @@
+public class Main {     
+public static void main(String[] args) {        
+ String str="Hello";         
+String rev=new StringBuilder(str).reverse().toString();        
+ System.out.println("Reversed = " + rev);     
+} 
+} 
